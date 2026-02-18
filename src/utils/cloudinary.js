9b89f98aeb2d97c07272c3f109b8f05ac0f01882
Khudi Ram Soren <khudiramsoren.07@gmail.com>
2026-02-18 23:07:09 +0530

    setup cloudinary and add to uploadMedia method

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,22 @@
+import { v2 as cloudinary } from "cloudinary";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_SECRET_KEY,
+});
+
+export const uploadMedia = async (file) => {
+  try {
+    const response = await cloudinary.uploader.upload(file, {
+      resource_type: "auto",
+    });
+    return response;
+  } catch (error) {
+    console.log("error in uploading media to cloudinary");
+    console.log(error);
+  }
+};
